d01b8935449387a4165355fa5a9bce87a28b2d45
Aylin <calaylin@gmail.com>
2014-07-21 12:02:33 -0400

    function that returns the line numbers that have the complete ast information for each functionID

--- SCAA/src/DepthASTNode.java
+++ SCAA/src/DepthASTNode.java
@@ -9,7 +9,56 @@ import org.apache.commons.lang3.StringUtils;
 
 public class DepthASTNode {
 	
-	
+    public static void main(String[] args) throws Exception, IOException, InterruptedException {
 
- 
+    	String fileName ="/Users/Aylin/Desktop/Drexel/2014/ARLInternship/"
+    			+ "SCAA_Datasets/AnalysisCode/for/"
+    			+ "simpleforlabels.dep";
+    	String featureText = Util.readFile( fileName);
+    	int[] lines =getASTDepLines(featureText);
+    	
+    	for(int i=0; i<lines.length; i++)
+    		System.out.println(lines[i]);
+    }
+	
+	public static int[] getASTDepLines(String featureText)
+	{		
+		HashSet<String> functionIDs = new HashSet<String>();
+		HashSet<String> functionIDs2 = new HashSet<String>();
+
+        //take the function id in the beginning of the line.    
+		String[] lines = featureText.split("\n");
+		for(int i=0; i< lines.length; i++)
+		{
+
+	        String firstWord = lines[i].substring(0, featureText.indexOf('\t'));
+	        if(!functionIDs.contains(firstWord))
+	        functionIDs.add(firstWord);
+		}
+		int [] ASTDepLines=new int[functionIDs.size()];
+		for(int i=0; i< lines.length; i++)
+		{
+	        String firstWord = lines[i].substring(0, featureText.indexOf('\t'));
+	        if(i==0)
+	        {
+		    functionIDs2.add(firstWord);
+	        }
+	        else
+	        {
+	        	if(!functionIDs2.contains(firstWord)||(i==lines.length-1))
+	        	{
+	        		int lineNumber = i-1;
+	        		ASTDepLines[functionIDs2.size()-1] = lineNumber;
+	        	}
+		    functionIDs2.add(firstWord);
+	        }
+
+		}       
+	
+	       return ASTDepLines;  
+       
+       
+       
+       
+	}
 }
