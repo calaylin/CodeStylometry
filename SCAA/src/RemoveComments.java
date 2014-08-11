958d36bc9b4bf9ef7fb36a1b8dea1a5466cf5917
Aylin <calaylin@gmail.com>
2014-08-11 11:22:15 -0400

    remove comments (// or /* */) from cpp code

---
+++ SCAA/src/RemoveComments.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.List;
+
+
+public class RemoveComments {
+
+    public static void main(String[] args) throws IOException
+	{
+    	String test = "foldername";	
+    	List test_file_paths = Util.listCPPFiles(test); //use this for preprocessing       
+    	for(int i=0; i< test_file_paths.size(); i++)
+    	{
+		String fileName = test_file_paths.get(i).toString();  
+		System.out.println(fileName);
+		String sourceCode = Util.readFile(fileName);    
+  //    System.out.println(sourceCode.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)",""));
+		//does not catch Gleb.kalachev's comments, removed them manually.  Has a lot of commented code.
+		Util.writeFile(sourceCode.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)",""), fileName, false);
+    	}
+	}
+    	
+}
