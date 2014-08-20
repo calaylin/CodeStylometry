5ed1bcc753306af7868e03accd7db427611b3e17
Andrew L <liu.andrew.x@gmail.com>
2014-08-20 10:17:29 -0400

    listed dependencies for running the ARFF maker

--- Naive Baseline/src/Driver.java
+++ Naive Baseline/src/Driver.java
@@ -1,9 +1,13 @@
-
 public class Driver {
-	
+
+	/**
+	 * Dependencies: Apache Commons IO, Util.java, ARFFFactory*.java, everything
+	 * else in the Naive-Baseline package
+	 */
 	public static void main(String args[]) {
 		if (args.length != 2) {
-			System.err.println("Usage: <root directory of all test files> <target ARFF file>");
+			System.err
+					.println("Usage: <root directory of all test files> <target ARFF file>");
 			System.exit(1);
 		}
 		(new ARFFFactory4()).makeARFF(args[0], args[1]);
