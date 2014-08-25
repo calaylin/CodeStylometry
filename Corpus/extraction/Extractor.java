aab42f5bfd4fc076fe6234f97187df224e24016d
Aylin <calaylin@gmail.com>
2014-08-25 15:26:27 -0400

    problem and contest numbers 2014

--- Corpus/extraction/Extractor.java
+++ Corpus/extraction/Extractor.java
@@ -1,3 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
 public class Extractor {
 
 	public static String getDownloadURL(String roundNum, String problemNum,
@@ -8,4 +18,67 @@ public class Extractor {
 				+ problemNum
 				+ "&io_set_id=0&username=" + name;
 	}
+	
+	
+    public static void main(String[] args) throws IOException{
+
+    	
+/*
+//http://code.google.com/codejam/contest/scoreboard/do?cmd=GetSourceCode&contest=[CONTESTNUMBER]&problem=[PROBLEMNUMBER]&io_set_id=[LARGEORSMALLDATASET]&username=[USERNAME]
+
+//The [LARGEORSMALLDATASET] field is either a 0 or 1 to download either the small or large dataset. Since we always use the small dataset, we can always leave this as 0. So for example, if I wanted the URL for the first problem (small dataset) in the 2014 Qualification round for Gennady.Korotkevich, my URL would be:
+
+//http://code.google.com/codejam/contest/scoreboard/do?cmd=GetSourceCode&contest=2974486&problem=5756407898963968&io_set_id=0&username=Gennady.Korotkevich
+*/
+
+    	int[] round_numbers_2014 ={2974486, 2984486, 2994486, 3004486, 3014486, 3024486};
+/*    	long[] problem_numbers_2014 = {5756407898963968L,5709773144064000L,5690574640250880L,5644738749267968L,
+	5634947029139456L,5766201229705216L,5752104073297920L,//round2
+	5751500831719424L,5658282861527040L,5731331665297408L,//round3
+	5706278382862336L,5669245564223488L,5658068650033152L,//round4
+	5737429512224768L,5721094409420800L,5158144455999488L,5649687893770240L,//round5
+	5645447150436352L,5724427840913408L,5690270771314688L,5670781216358400L};//round6
+*/    	
+    	long[][] problem_numbers_2014 = new long[6][4];
+    	problem_numbers_2014[0][0] = 5756407898963968L;
+    	problem_numbers_2014[0][1] = 5709773144064000L;
+    	problem_numbers_2014[0][2] = 5690574640250880L;
+    	problem_numbers_2014[0][3] = 5644738749267968L;
+    	problem_numbers_2014[1][0] = 5634947029139456L;
+    	problem_numbers_2014[1][1] = 5766201229705216L;
+    	problem_numbers_2014[1][2] = 5752104073297920L;
+    	problem_numbers_2014[2][0] = 5751500831719424L;
+    	problem_numbers_2014[2][1] = 5658282861527040L;
+    	problem_numbers_2014[2][2] = 5731331665297408L;
+    	problem_numbers_2014[3][1] = 5706278382862336L;
+    	problem_numbers_2014[3][2] = 5669245564223488L;
+    	problem_numbers_2014[3][3] = 5658068650033152L;
+    	problem_numbers_2014[4][1] = 5737429512224768L;
+    	problem_numbers_2014[4][2] = 5721094409420800L;
+    	problem_numbers_2014[4][3] = 5158144455999488L;
+    	problem_numbers_2014[4][4] = 5649687893770240L;
+    	problem_numbers_2014[5][1] = 5645447150436352L;
+    	problem_numbers_2014[5][2] = 5724427840913408L;
+    	problem_numbers_2014[5][3] = 5690270771314688L;
+    	problem_numbers_2014[5][4] = 5670781216358400L;
+
+    			
+
+    			
+    			
+    			
+    			
+    	BufferedReader in = new BufferedReader(new FileReader("users/2014_fromDB.txt"));
+        String str;
+
+        List<String> list = new ArrayList<String>();
+        while((str = in.readLine()) != null){
+            list.add(str);
+        }
+
+        String[] stringArr = list.toArray(new String[0]);    	
+        for(int i=0; i< stringArr.length; i++)
+        	System.out.println(stringArr[i]);
+        }
+
 }
