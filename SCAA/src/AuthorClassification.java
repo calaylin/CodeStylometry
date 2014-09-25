477066e333bb1cb200b9c04848c755b503f0d86d
Aylin <calaylin@gmail.com>
2014-09-25 19:05:52 -0400

    won't include JStylo

--- SCAA/src/AuthorClassification.java
+++ SCAA/src/AuthorClassification.java
@@ -1,15 +1,11 @@
 import weka.classifiers.*;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
-import edu.drexel.psal.jstylo.analyzers.WekaAnalyzer;
-import edu.drexel.psal.jstylo.generics.*;
-
 import java.io.FileReader;
 import java.util.*;
 
 public class AuthorClassification {
 
-	public static MultiplePrintStream out;
 
 	public static void main(String[] args) throws Exception 
 	{
@@ -17,7 +13,6 @@ public class AuthorClassification {
 		int numberFiles=2;
 		int seedNumber=7;
 		int foldNumber=2;
-		int relaxParam=2;
 		String arffFile = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/SCAAarffs/mergedArffs/incremental/"+numberFiles+"files2014FS9Andrew.arff";		 
 		RandomForest cls = new RandomForest();
 		Instances data = new Instances(new FileReader(arffFile));
@@ -28,52 +23,22 @@ public class AuthorClassification {
 		cls.setOptions(options);
 		cls.buildClassifier(data);
 		
-/*		
+	
 	    Evaluation eval = new Evaluation(data);
 	    eval.crossValidateModel(cls, data,foldNumber , new Random(seedNumber));
         System.out.println(eval.toSummaryString("\nResults\n", true));
         System.out.println(eval.fMeasure(1) + " " + eval.precision(1) + " " + eval.recall(1));
-*/
+
 	        
-		WekaDetailedAnalyzer wa = new WekaDetailedAnalyzer(cls,relaxParam);
-		Evaluation eval = wa.runCrossValidation(data, foldNumber, seedNumber);
 		System.out.println("done! results:");
 		System.out.println(eval.toSummaryString());
 		System.out.println(eval.toClassDetailsString());
 		//out.println(eval.toMatrixString());
- //       System.out.println(eval.fMeasure(1) + " " + eval.precision(1) + " " + eval.recall(1));
 		System.out.println();
-	}
+	
 		 
 
 
-	public static class WekaDetailedAnalyzer extends WekaAnalyzer
-	{
-		protected Classifier cls;
-		protected int relaxParam;
-		protected boolean relaxed;
-		
-		public static MultiplePrintStream out;
-		
-		public WekaDetailedAnalyzer(Classifier classifier) {
-			super(classifier);
-			cls = classifier;
-			if (out == null)
-				out = AuthorClassification.out;
-		}
-		
-		public WekaDetailedAnalyzer(Classifier classifier, int relaxParam) {
-			super(classifier);
-			cls = classifier;
-			if (out == null)
-				out = AuthorClassification.out;
-			if (relaxParam < 1)
-				relaxed = false;
-			else
-			{
-				relaxed = true;
-				this.relaxParam = relaxParam;
-			}
-		}
 	}
+	
 }
