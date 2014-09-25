39f8d22a92bfeea4e02648aabb37cffa249aa8a8
Aylin <calaylin@gmail.com>
2014-09-25 18:13:37 -0400

    RF

--- SCAA/src/AuthorClassification.java
+++ SCAA/src/AuthorClassification.java
@@ -1,15 +1,8 @@
 
 import weka.classifiers.*;
-import weka.classifiers.functions.*;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
-import weka.filters.Filter;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.lang.management.ManagementFactory;
-import java.nio.charset.Charset;
 import java.util.*;
 
 public class AuthorClassification {
@@ -22,15 +15,17 @@ public class AuthorClassification {
 		int seedNumber=7;
 		int foldNumber=10;
 		String arffFile = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/SCAAarffs/mergedArffs/incremental/"+numberFiles+"filesDifficult2014FS9Andrew.arff";		 
-		Classifier cls = new RandomForest();
+		RandomForest cls = new RandomForest();
 		Instances data = new Instances(new FileReader(arffFile));
 		data.setClassIndex(data.numAttributes() - 1);
 		System.out.println("number of authors: " + data.numClasses());
-		 
-		cls.buildClassifier(data);
-		System.out.println(cls);
 		String[] options = weka.core.Utils.splitOptions("-I 300 -K 0 -S "+seedNumber);
 
+		cls.setOptions(options);
+		cls.buildClassifier(data);
+		System.out.println(cls);
+		
+		
 	    Evaluation eval = new Evaluation(data);
 	    eval.crossValidateModel(cls, data,foldNumber , new Random(seedNumber));
         System.out.println(eval.toSummaryString("\nResults\n", true));
