929dab03af9d7eff23186639f3243a5fda1496ed
Aylin <calaylin@gmail.com>
2014-09-25 21:53:14 -0400

    write results to file

--- SCAA/src/AuthorClassification.java
+++ SCAA/src/AuthorClassification.java
@@ -14,12 +14,12 @@ public class AuthorClassification {
 
 		int numberFiles;
 		int seedNumber;
-		for(numberFiles=9; numberFiles<14; numberFiles++){
+		for(numberFiles=8; numberFiles<14; numberFiles++){
 
 		for(seedNumber=8; seedNumber<11; seedNumber++){
 			int foldNumber=numberFiles;
 
-		String arffFile = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/SCAAarffs/mergedArffs/incremental/"+numberFiles+"files2014FS9Andrew.arff";		 
+		String arffFile = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/SCAAarffs/mergedArffs/incremental/"+numberFiles+"files2014FS9Andrew_ready.arff";		 
 		RandomForest cls = new RandomForest();
 		Instances data = new Instances(new FileReader(arffFile));
 		data.setClassIndex(data.numAttributes() - 1);
@@ -33,11 +33,11 @@ public class AuthorClassification {
 	    Evaluation eval = new Evaluation(data);
 	    eval.crossValidateModel(cls, data,foldNumber , new Random(seedNumber));
 		System.out.println("seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors");
-		Util.writeFile(		"seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors",
+		Util.writeFile(		"seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors"+"\n",
 				"/Users/Aylin/Desktop/resultsSep25.txt", true);
 
 	     System.out.println("Correctly classified instances: "+eval.pctCorrect());
-	     Util.writeFile("Correctly classified instances: "+eval.pctCorrect(), "/Users/Aylin/Desktop/resultsSep25.txt", true);	
+	     Util.writeFile("Correctly classified instances: "+eval.pctCorrect()+"\n", "/Users/Aylin/Desktop/resultsSep25.txt", true);	
 //        System.out.println(eval.toSummaryString("\nResults\n", true));
 //        System.out.println(eval.fMeasure(1) + " " + eval.precision(1) + " " + eval.recall(1));
 	        
