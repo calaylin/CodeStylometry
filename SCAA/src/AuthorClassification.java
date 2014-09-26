d63e11e6d2e674ef06d7f170b146aa96469da613
Aylin <calaylin@gmail.com>
2014-09-25 21:19:05 -0400

    trainingData size

--- SCAA/src/AuthorClassification.java
+++ SCAA/src/AuthorClassification.java
@@ -2,6 +2,7 @@
 import weka.classifiers.*;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
+
 import java.io.FileReader;
 import java.util.*;
 
@@ -11,15 +12,18 @@ public class AuthorClassification {
 	public static void main(String[] args) throws Exception 
 	{
 
-		int numberFiles=8;
-		int seedNumber=7;
-		int foldNumber=numberFiles;
-		for(seedNumber=7; seedNumber<11; seedNumber++){
+		int numberFiles;
+		int seedNumber;
+		for(numberFiles=9; numberFiles<14; numberFiles++){
+
+		for(seedNumber=8; seedNumber<11; seedNumber++){
+			int foldNumber=numberFiles;
+
 		String arffFile = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/SCAAarffs/mergedArffs/incremental/"+numberFiles+"files2014FS9Andrew.arff";		 
 		RandomForest cls = new RandomForest();
 		Instances data = new Instances(new FileReader(arffFile));
 		data.setClassIndex(data.numAttributes() - 1);
-		System.out.println("number of authors: " + data.numClasses());
+		System.out.println("Number of authors: " + data.numClasses());
 		String[] options = weka.core.Utils.splitOptions("-I 300 -K 0 -S "+seedNumber);
 
 		cls.setOptions(options);
@@ -28,13 +32,18 @@ public class AuthorClassification {
 		
 	    Evaluation eval = new Evaluation(data);
 	    eval.crossValidateModel(cls, data,foldNumber , new Random(seedNumber));
-       // System.out.println(eval.toSummaryString("\nResults\n", true));
+		System.out.println("seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors");
+		Util.writeFile(		"seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors",
+				"/Users/Aylin/Desktop/resultsSep25.txt", true);
+
+	     System.out.println("Correctly classified instances: "+eval.pctCorrect());
+	     Util.writeFile("Correctly classified instances: "+eval.pctCorrect(), "/Users/Aylin/Desktop/resultsSep25.txt", true);	
+//        System.out.println(eval.toSummaryString("\nResults\n", true));
 //        System.out.println(eval.fMeasure(1) + " " + eval.precision(1) + " " + eval.recall(1));
-System.out.println("etsttet");
 	        
 	      
-		}	 
-
+			}	 
+		}
 
 	}
 	
