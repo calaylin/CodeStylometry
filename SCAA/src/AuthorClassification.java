571fc3ea18145c6af51e7f217bdc424bb6dc6396
Aylin <calaylin@gmail.com>
2014-09-26 08:17:11 -0400

    Relaxed attribution code added for my experiments

--- SCAA/src/AuthorClassification.java
+++ SCAA/src/AuthorClassification.java
@@ -1,4 +1,3 @@
-
 import weka.classifiers.*;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
@@ -9,38 +8,50 @@ import java.util.*;
 public class AuthorClassification {
 
 
+	@SuppressWarnings("null")
 	public static void main(String[] args) throws Exception 
 	{
 
+		int endRelax = 3;
 		int numberFiles;
 		int seedNumber;
-		for(numberFiles=8; numberFiles<14; numberFiles++){
+		for(numberFiles=9; numberFiles<10; numberFiles++){
 
-		for(seedNumber=8; seedNumber<11; seedNumber++){
+		for(seedNumber=8; seedNumber<9; seedNumber++){
 			int foldNumber=numberFiles;
 
 		String arffFile = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/SCAAarffs/mergedArffs/incremental/"+numberFiles+"files2014FS9Andrew_ready.arff";		 
 		RandomForest cls = new RandomForest();
 		Instances data = new Instances(new FileReader(arffFile));
 		data.setClassIndex(data.numAttributes() - 1);
+		data.stratify(foldNumber);
 		System.out.println("Number of authors: " + data.numClasses());
 		String[] options = weka.core.Utils.splitOptions("-I 300 -K 0 -S "+seedNumber);
 
 		cls.setOptions(options);
 		cls.buildClassifier(data);
+		Evaluation eval=null;
 		
 		
-	    Evaluation eval = new Evaluation(data);
-	    eval.crossValidateModel(cls, data,foldNumber , new Random(seedNumber));
-		System.out.println("seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors");
-		Util.writeFile(		"seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors"+"\n",
-				"/Users/Aylin/Desktop/resultsSep25.txt", true);
-
-	     System.out.println("Correctly classified instances: "+eval.pctCorrect());
-	     Util.writeFile("Correctly classified instances: "+eval.pctCorrect()+"\n", "/Users/Aylin/Desktop/resultsSep25.txt", true);	
-//        System.out.println(eval.toSummaryString("\nResults\n", true));
-//        System.out.println(eval.fMeasure(1) + " " + eval.precision(1) + " " + eval.recall(1));
-	        
+		if(endRelax==1)
+		 eval = new Evaluation(data);
+		else
+		 eval= new RelaxedEvaluation(data, endRelax);
+		
+		
+		
+		eval.crossValidateModel(cls, data,foldNumber , new Random(seedNumber));
+			
+		System.out.println("Relaxed by: "+endRelax+", seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors");
+		Util.writeFile("Relaxed by: "+endRelax+", seedNo:"+seedNumber+", "+numberFiles+" files per each of the "+data.numClasses()+" authors"+"\n",
+				"/Users/Aylin/Desktop/relaxedresultsSep25.txt", true);
+	     
+			
+		System.out.println("Relaxed by: "+endRelax+", Correctly classified instances: "+eval.pctCorrect()+" OOB error: "+cls.measureOutOfBagError());
+	     Util.writeFile("Relaxed by: "+endRelax+", Correctly classified instances: "+eval.pctCorrect()+" OOB error: "+cls.measureOutOfBagError()+"\n",
+	    		 "/Users/Aylin/Desktop/relaxedresultsSep25.txt", true);	
+	     
+				
 	      
 			}	 
 		}
