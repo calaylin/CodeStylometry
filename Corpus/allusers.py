2e422636c99f49dde95942cf6071475cd14b904b
Andrew Liu <liu.andrew.x@gmail.com>
2015-01-14 21:36:12 -0500

    cleaned up scripts

--- Corpus/allusers.py
+++ Corpus/allusers.py
@@ -5,7 +5,6 @@ users = {}
 
 def get_all_users(round_id, num_players):
 	global users
-	counter = 1
 	for pos in range(1, int(num_players), 30):
 		meta_url = "http://code.google.com/codejam/contest/scoreboard/" \
 			+ "do?cmd=GetScoreboard&contest_id=" \
@@ -16,27 +15,18 @@ def get_all_users(round_id, num_players):
 		meta_url_data = urlopen(meta_url).read()
 		meta_json = json.loads(meta_url_data)
 		for row in meta_json['rows']:
-			#users.append(row['n'])
 			username = row['n']
 			users[username] = True
-			print "added user " + str(counter)
-			counter += 1
-	#return users
 
 user_file = open('users.txt', 'w')
 metadatafile = open("CodeJamMetadata.json").read()
 metadata = json.loads(metadatafile)
 for year_json in metadata['competitions']:
-	#for round_json in year_json['round']:
-		#round_id = round_json['contest']
 	qual_round = year_json['round'][0]
 	num_players = qual_round['numPlayers']
 	round_id = qual_round['contest']
 	get_all_users(round_id, num_players)
-	#
 for user in users.keys():
-	print user
 	user_file.write(user)
 	user_file.write('\n')
-	#
 user_file.close()
--- Corpus/cjcompile.py
+++ Corpus/cjcompile.py
@@ -1,47 +1,10 @@
 import os
 import sys
 
-# os.listdir('.') --> list files in cwd
-# os.path.isfile(f) --> if f is a file vs isdir
-
-# os.system("ls -l")
-
-# os.path.splitext(filename)[0] --> get filename w/o extension
-# gcc test2/test.c -o test2/testout
-
-# sys.argv == list of args
-# len(sys.argv) == number of args (including file)
-# example...
-# python test.py arg1 args2 arg3
-# sys.argv == ['test.py', 'arg1', 'arg2', 'arg3']
-
-# make a stack
-# add cwd to stack
-# while stack has contents
-	# cwd = stack.pop
-	# get list of contents in cwd
-	# for each item in the list
-		# if it's a directory, add it to the stack
-		# if it's a c/cpp file then compile it
-'''
-directories = []
-directories.append(os.getcwd())
-while len(directories) > 0:
-	directory = directories.pop()
-	for my_file in os.listdir(directory):
-		if os.path.isdir(my_file): # will not work...listdir gives relative path -> need full path name
-			directories.append(my_file)
-		elif os.path.isfile(my_file) and my_file.endswith(('.c', '.cpp')):
-			#compile
-'''
-'''
-for (path, dirs, files) in os.walk('.'):
-	print path # directory path
-	print dirs # list of folders
-	print files # list of files
-'''
 myargs = sys.argv
 myargs.pop(0)
+
+# go through all files under the root directorys
 for (path, dirs, files) in os.walk('.'):
 	for f in files:
 		if f.endswith(('.c', '.cpp')):
@@ -55,13 +18,9 @@ for (path, dirs, files) in os.walk('.'):
 			command += " -o "
 			command += path
 			command += '/'
-			command += os.path.splitext(f)[0]
+			command += os.path.splitext(f)[0] # remove file extension
 			for arg in myargs:
 				command += " "
 				command += arg
 			os.system(command)
 			print command
-'''
-for myarg in sys.argv:
-	print myarg
-'''
