590a12d3347108a42989abe060c4a07a6ff4b504
Andrew Liu <liu.andrew.x@gmail.com>
2015-01-14 21:53:36 -0500

    documented cjcompile.py

--- Corpus/cjcompile.py
+++ Corpus/cjcompile.py
@@ -1,26 +1,42 @@
 import os
 import sys
 
-myargs = sys.argv
-myargs.pop(0)
+#
+# Usage: python /directory/path/to/cjcompile.py [compiler flags]
+#
+# Compiles all C/C++ source files in the current working directory.
+# Also recursively compiles all C/C++ source files in all subdirectories.
+#
+
+flags = sys.argv
+flags.pop(0)
 
 # go through all files under the root directorys
 for (path, dirs, files) in os.walk('.'):
 	for f in files:
+		# check if file is a C or C++ file
 		if f.endswith(('.c', '.cpp')):
-			if f.endswith('.c'):
+			if f.endswith('.c'): # C file
 				command = "gcc "
-			else:
+			else: # C++ file
 				command = "g++ "
+
+			# add full directory path of the source file
 			command += path
 			command += '/'
 			command += f
+
+			# add full directory path of the executable
 			command += " -o "
 			command += path
 			command += '/'
 			command += os.path.splitext(f)[0] # remove file extension
-			for arg in myargs:
+
+			# adding in compiler flags (specified in the arguments)
+			for flag in flags:
 				command += " "
-				command += arg
+				command += flag
+
+			# compile
 			os.system(command)
 			print command
