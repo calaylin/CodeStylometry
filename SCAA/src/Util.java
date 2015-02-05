ac1e336b9356ff0b05276e1af5cd125ec326dcfe
Aylin <calaylin@gmail.com>
2015-02-05 08:38:28 -0500

    sort by line

--- SCAA/src/Util.java
+++ SCAA/src/Util.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,6 +12,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -495,4 +497,33 @@ public class Util {
         	        System.out.println("File copied from " + src + " to " + dest);
         	}
         }
+    
+
+
+    	public static void AlphabeticallySortLinesOfTextInFile(String inputFile, String outputFile) throws IOException{
+
+
+    		FileReader fileReader = new FileReader(inputFile);
+    		BufferedReader bufferedReader = new BufferedReader(fileReader);
+    		String inputLine;
+    		List<String> lineList = new ArrayList<String>();
+    		while ((inputLine = bufferedReader.readLine()) != null) {
+    			lineList.add(inputLine);
+    		}
+    		fileReader.close();
+
+    		Collections.sort(lineList);
+
+    		FileWriter fileWriter = new FileWriter(outputFile);
+    		PrintWriter out = new PrintWriter(fileWriter);
+    		for (String outputLine : lineList) {
+    			out.println(outputLine);
+    		}
+    		out.flush();
+    		out.close();
+    		fileWriter.close();
+
+    	}
 }
+
+
