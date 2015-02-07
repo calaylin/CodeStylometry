a7d06f7bdc2b1f4634086f77a3d504e97109646a
Aylin <calaylin@gmail.com>
2015-02-07 16:20:12 -0500

    within author distances

--- SCAA/src/LevenshteinDistance.java
+++ SCAA/src/LevenshteinDistance.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 import com.csvreader.CsvWriter;
@@ -12,6 +15,9 @@ public class LevenshteinDistance {
  //   s1 = s1.toLowerCase();
  //   s2 = s2.toLowerCase();
  
+	  System.out.println("Length of first string: "+s1.length());
+	  System.out.println("Length of second string: "+s2.length());
+
     int[] costs = new int[s2.length() + 1];
     for (int i = 0; i <= s1.length(); i++) {
       int lastValue = i;
@@ -43,16 +49,61 @@ public class LevenshteinDistance {
   @SuppressWarnings("resource")
 public static void main(String[] args) throws IOException {
 	  
+	  	String parentDir = "/Users/Aylin/Desktop/Drexel/2014/ARLInternship/"
+				+ "SCAA_Datasets/bigExperiments/250authors/9FilesExactlyPer250Author_2014/";
+	   	String output_file= "/Users/Aylin/Desktop/similarityTest.txt";
 
-	// Construct a BufferedReader object from the input file
-		String file1 = Util.readFile("/Users/Aylin/Desktop/Drexel/2014/ARLInternship/"
-				+ "SCAA_Datasets/bigExperiments/250authors/9FilesExactlyPer250Author_2014/"
-				+ "PlutoShe/2974486_5644738749267968_PlutoShe.cpp");
-		String file2 = Util.readFile("/Users/Aylin/Desktop/Drexel/2014/ARLInternship/"
-				+ "SCAA_Datasets/bigExperiments/250authors/9FilesExactlyPer250Author_2014/"
-				+ "PlutoShe/2974486_5690574640250880_PlutoShe.cpp");
-	  
-	  printDistance(file1, file2);
+	  File file = new File(parentDir);
+	   String[] directories = file.list(new FilenameFilter() 
+	   {
+	     @Override
+	     public boolean accept(File current, String name) 
+	     {
+	       return new File(current, name).isDirectory();
+	     }
+	   });
+	   System.out.println(Arrays.toString(directories));	
+	   
+	   for (int i =0; i< directories.length; i++)
+	   {
+		   //authorname is directoryname - 1 because Andrew put an extra 0 at the end of the authorname
+		   String authorName = directories[i].toString().substring(0, directories[i].toString().length()); 
+		   String authorDir = parentDir + directories[i] + "/";
+		   Util.writeFile(authorName +"\n", output_file, true);
+		   System.out.println(authorName);
+		   System.out.println(authorDir);
 
-  }
+		   BufferedReader br = null;
+			String line = "";
+		   	List test_cpp_paths = Util.listCPPFiles(authorDir);
+		   	for(int j=0; j < test_cpp_paths.size();j++ )
+			{
+				String file1 = Util.readFile(test_cpp_paths.get(j).toString());
+				   Util.writeFile(test_cpp_paths.get(j).toString() +"\n", output_file, true);
+				   double  avg_ratio=0;
+				for(int k=0; k < test_cpp_paths.size();k++ )
+				{
+					double ratio =0;
+					if(j!=k){
+						String file2 = Util.readFile(test_cpp_paths.get(k).toString());
+						int distance =computeDistance(file1, file2);
+						if(file1.length() <= file2.length()){
+							 ratio = distance/(double)((Integer)file2.length());
+						}
+						if(file2.length() < file1.length()){
+							 ratio = distance/(double)((Integer)file1.length());
+						}	
+						
+						Util.writeFile("File1 length: "+file1.length() +" " +
+								"File2 length:"+file2.length()+" ", output_file, true);
+						Util.writeFile("distance: "+Integer.toString(distance)+
+								" "+"ratio: "+ Double.toString(ratio) + "\n" ,output_file, true);
+						avg_ratio =+ratio;
+					}}
+					avg_ratio = avg_ratio/(double)((Integer)(test_cpp_paths.size()-1));
+					   Util.writeFile("average ratio of file: "+avg_ratio +"\n", output_file, true);
+
+				}
+			}	  
+	   }  
 }
