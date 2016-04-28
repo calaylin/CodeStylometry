f5ff470bafc9c4667d4d97bce3791ae01b1a6801
aylin <aylin@nat-tvwna-inside-visitornet101-a-29266.princeton.org>
2016-04-27 20:14:14 -0400

    func

--- SCAA/src/RemoveComments.java
+++ SCAA/src/RemoveComments.java
@@ -6,7 +6,7 @@ public class RemoveComments {
 
     public static void main(String[] args) throws IOException
 	{
-    	String test = "foldername";	
+    	String test = "githubManySmallSnippets/";	
     	List test_file_paths = Util.listCPPFiles(test); //use this for preprocessing       
     	for(int i=0; i< test_file_paths.size(); i++)
     	{
@@ -16,7 +16,20 @@ public class RemoveComments {
   //    System.out.println(sourceCode.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)",""));
 		//does not catch Gleb.kalachev's comments, removed them manually.  Has a lot of commented code.
 		Util.writeFile(sourceCode.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)",""), fileName, false);
-    	}
+    	}}
+        public static void removeComments(String test) throws IOException
+    	{
+        	List test_file_paths = Util.listCPPFiles(test); //use this for preprocessing       
+        	for(int i=0; i< test_file_paths.size(); i++)
+        	{
+    		String fileName = test_file_paths.get(i).toString();  
+    		System.out.println(fileName);
+    		String sourceCode = Util.readFile(fileName);    
+      //    System.out.println(sourceCode.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)",""));
+    		//does not catch Gleb.kalachev's comments, removed them manually.  Has a lot of commented code.
+    		Util.writeFile(sourceCode.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)",""), fileName, false);
+        	}	
+    	
 	}
     	
 }
